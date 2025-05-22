439422d8f0479726765e9a769b48c1ac6297a74f
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2025-05-22 12:40:46 +0200

    trying more starts, less kept

--- src/blikelihood.jl
+++ src/blikelihood.jl
@@ -152,7 +152,7 @@ function both_loglike(theta, resp, pred, dimvals, ranks)
     return 0.5 * ((obs - 1) * logdet_term + sse)
 end
 
-function comovement_init(resp, pred, dimvals, ranks; iters=3, tol=1e-05, num_starts=30, num_selected=10)
+function comovement_init(resp, pred, dimvals, ranks; iters=5, tol=1e-05, num_starts=50, num_selected=5)
     some_init = init_both(resp, pred, dimvals, ranks)
     init_length = length(some_init)
     potential_starts = fill(NaN, init_length + 1, num_starts)
@@ -197,13 +197,13 @@ function comovement_init(resp, pred, dimvals, ranks; iters=3, tol=1e-05, num_sta
 
 end
 
-function main_algorithm(resp, pred, dimvals, ranks; iters=100, tol=1e-05, num_starts=30, num_selected=10)
+function main_algorithm(resp, pred, dimvals, ranks; iters=100, tol=1e-05, num_starts=50, num_selected=5)
 
     obj = tet -> both_loglike(tet, resp, pred, dimvals, ranks)
     td = nothing
     res = nothing
 
-    chosen_start, num_iters, problem_starts = comovement_init(resp, pred, dimvals, ranks; iters=3, tol=1e-01, num_starts, num_selected)
+    chosen_start, num_iters, problem_starts = comovement_init(resp, pred, dimvals, ranks; iters=5, tol=1e-01, num_starts, num_selected)
     potential_results = Vector{Any}(undef, size(chosen_start, 2))
 
     count = 0
@@ -236,7 +236,7 @@ function main_algorithm(resp, pred, dimvals, ranks; iters=100, tol=1e-05, num_st
     return (; res, td, num_iters, problem_starts)
 end
 
-function comovement_reg(data, dimvals, ranks; iters=100, tol=1e-05, num_starts=30, num_selected=10)
+function comovement_reg(data, dimvals, ranks; iters=100, tol=1e-05, num_starts=50, num_selected=5)
 
     perm_mat = both_perm_mat(dimvals, ranks)
     perm_resp = (perm_mat*data)[:, 2:end]
