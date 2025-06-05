8678849ed9e476319c3fa0ccb3220fc73760f9da
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2025-06-05 07:07:21 +0200

    trying new

--- scripts/simulations/coverage/delta_cov250.jl
+++ scripts/simulations/coverage/delta_cov250.jl
@@ -1,6 +1,6 @@
 using DrWatson
 @quickactivate :PseudoStructuralComovements
-Random.seed!(20250530)
+Random.seed!(20250605)
 
 dimvals = [3, 4]
 true_ranks = [2, 2]
--- scripts/simulations/coverage/more_lags.jl
+++ scripts/simulations/coverage/more_lags.jl
@@ -31,8 +31,8 @@ over_cov = fill(NaN, 2, sims)
     cen_data = data.data .- mean(data.data, dims=2)
 
     correct_reg = comovement_reg(cen_data, dimvals, true_ranks; iters=100, p)
-    over_reg = comovement_reg(cen_data, dimvals, over_rank; iters=100)
-    under_reg = comovement_reg(cen_data, dimvals, under_rank; iters=100)
+    over_reg = comovement_reg(cen_data, dimvals, over_rank; iters=100, p)
+    under_reg = comovement_reg(cen_data, dimvals, under_rank; iters=100, p)
 
     correct_delta[:, i] = correct_reg.delta_est[2:end]
     under_delta[:, i] = under_reg.delta_est[2:end]
