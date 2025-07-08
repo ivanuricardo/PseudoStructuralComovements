48fdfa532b5df988783549a5a59fd8d6d8df97ec
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2025-07-08 17:48:54 +0200

    more sim results

---
+++ scripts/simulations/coverage/34/severe_case.jl
@@ -0,0 +1,52 @@
+using DrWatson
+@quickactivate :PseudoStructuralComovements
+Random.seed!(20250607)
+
+dimvals = [3, 4]
+true_rank = [2, 4]
+under_rank = [2, 2]
+
+sims = 1000
+burnin = 50
+obs = 250 + burnin
+
+coef = generate_rrmar_coef(dimvals, true_rank)
+delta_true = coef.delta
+gamma_true = coef.gamma
+u3_true = coef.u3
+u4_true = coef.u4
+
+correct_delta = fill(NaN, 2, sims)
+under_delta = fill(NaN, 2, sims)
+
+correct_cov = fill(NaN, 2, sims)
+under_cov = fill(NaN, 2, sims)
+
+@showprogress Threads.@threads for i = 1:sims
+    data = simulate_rrmar_data(dimvals, true_rank, obs; A=coef, burnin, snr=0.4)
+    cen_data = data.data .- mean(data.data, dims=2)
+
+    correct_reg = comovement_reg(cen_data, dimvals, true_rank; iters=1000)
+    under_reg = comovement_reg(cen_data, dimvals, under_rank; iters=1000)
+
+    correct_delta[:, i] = correct_reg.delta_est[2:end]
+    under_delta[:, i] = under_reg.delta_est[2:end]
+
+    correct_upper = correct_reg.delta_est[2:end] + 1.96 .* correct_reg.delta_stderr
+    correct_lower = correct_reg.delta_est[2:end] - 1.96 .* correct_reg.delta_stderr
+    correct_cov[:, i] = correct_lower .< delta_true[2:end] .< correct_upper
+
+    under_upper = under_reg.delta_est[2:end] + 1.96 .* under_reg.delta_stderr
+    under_lower = under_reg.delta_est[2:end] - 1.96 .* under_reg.delta_stderr
+    under_cov[:, i] = under_lower .< delta_true[2:end] .< under_upper
+
+end
+
+save(datadir("coverage/severe_case.jld2"), Dict(
+    "correct_delta" => correct_delta,
+    "under_delta" => under_delta,
+    "correct_cov" => correct_cov,
+    "under_cov" => under_cov,
+    "delta_true" => delta_true
+))
+
