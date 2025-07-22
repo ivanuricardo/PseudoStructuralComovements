29ff80eebe56bf24e05df782923ba913711fdc6c
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2025-07-22 16:20:35 +0200

    add coverage results

--- plots/density_scripts/34/delta_density100.jl
+++ plots/density_scripts/34/delta_density100.jl
@@ -8,15 +8,33 @@ under_delta = density_results["under_delta"]
 over_delta = density_results["over_delta"]
 delta_true = density_results["delta_true"]
 
+correct_cov = density_results["correct_cov"]
+under_cov = density_results["under_cov"]
+over_cov = density_results["over_cov"]
+
 h1 = density(correct_delta[1, :], legend=false, ylabel="Density")
 density!(under_delta[1, :])
 density!(over_delta[1, :])
 vline!([delta_true[2]])
 savefig(h1, "../../density_plots/34/delta_density100_1.png")
 
-h2 = density(correct_delta[2, :], legend=false)
-density!(under_delta[2, :])
-density!(over_delta[2, :])
-vline!([delta_true[3]])
+h2 = density(correct_delta[2, :], label="Correct gamma", legendfontsize=9)
+density!(under_delta[2, :], label="Underestimated gamma")
+density!(over_delta[2, :], label="Overestimated gamma")
+vline!([delta_true[3]], label="True value")
 savefig(h2, "../../density_plots/34/delta_density100_2.png")
 
+correct_coverages = sum(correct_cov, dims=2)
+over_coverages = sum(over_cov, dims=2)
+under_coverages = sum(under_cov, dims=2)
+
+println("First delta estimate coverages:")
+println("given correct gamma rank: $(correct_coverages[1])")
+println("given overestimated gamma rank: $(over_coverages[1])")
+println("given underestimated gamma rank: $(under_coverages[1])")
+println("_________________________________________")
+
+println("Second delta estimate coverages:")
+println("given correct gamma rank: $(correct_coverages[2])")
+println("given overestimated gamma rank: $(over_coverages[2])")
+println("given underestimated gamma rank: $(under_coverages[2])")
--- plots/density_scripts/34/gamma_density250.jl
+++ plots/density_scripts/34/gamma_density250.jl
@@ -8,6 +8,10 @@ under_gamma = density_results["under_gamma"]
 over_gamma = density_results["over_gamma"]
 gamma_true = density_results["gamma_true"]
 
+correct_cov = density_results["correct_cov"]
+under_cov = density_results["under_cov"]
+over_cov = density_results["over_cov"]
+
 h1 = density(correct_gamma[1, :], legend=false, ylabel="Density")
 density!(under_gamma[1, :])
 density!(over_gamma[1, :])
@@ -25,3 +29,24 @@ density!(under_gamma[3, :], label="Underestimated rank")
 density!(over_gamma[3, :], label="Overestimated rank")
 vline!([gamma_true[4]], label="True value")
 savefig(h3, "../../density_plots/34/gamma_density250_3.png")
+
+correct_coverages = sum(correct_cov, dims=2)
+over_coverages = sum(over_cov, dims=2)
+under_coverages = sum(under_cov, dims=2)
+
+println("First gamma estimate coverages:")
+println("given correct delta rank: $(correct_coverages[1])")
+println("given overestimated delta rank: $(over_coverages[1])")
+println("given underestimated delta rank: $(under_coverages[1])")
+println("_________________________________________")
+
+println("Second gamma estimate coverages:")
+println("given correct delta rank: $(correct_coverages[2])")
+println("given overestimated delta rank: $(over_coverages[2])")
+println("given underestimated delta rank: $(under_coverages[2])")
+println("_________________________________________")
+
+println("Third gamma estimate coverages:")
+println("given correct delta rank: $(correct_coverages[3])")
+println("given overestimated delta rank: $(over_coverages[3])")
+println("given underestimated delta rank: $(under_coverages[3])")
