7a97ff30012593177a0a8a92f68d25cb4f1c8946
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2025-07-23 11:10:42 +0200

    data is prepped

--- data/country_indicators/globaldataclean.jl
+++ data/country_indicators/globaldataclean.jl
@@ -2,10 +2,12 @@ using DrWatson
 @quickactivate
 using CSV, DataFrames, JLD2
 
-function cleancountry(country::String, df::DataFrame; logtransform::Bool=false, difftransform::Bool=false)
+function cleancountry(country::String, df::DataFrame; logtransform::Bool=false, diffval::Int=1)
     country_data = filter(row -> row.Location == country, df)
     vals = logtransform ? log.(country_data.Value) : country_data.Value
-    diffvalues = difftransform ? diff(vals) : vals
+    if diffval > 0
+        diffvalues = vals[diffval+1:end] .- vals[1:end-diffval]
+    end
     return diffvalues
 end
 
@@ -14,31 +16,26 @@ function seasonal_diff_log(x::Vector, s::Int=4)
 end
 
 countries = ["USA", "CAN", "DEU", "FRA", "GBR"]
-#=countries = ["USA", "CAN", "DEU", "FRA"]=#
 
 interestrate = CSV.read(datadir("./country_indicators/ir.csv"), DataFrame)
 gdp = CSV.read(datadir("./country_indicators/gdp.csv"), DataFrame)
 production = CSV.read(datadir("./country_indicators/production.csv"), DataFrame)
 cpi = CSV.read(datadir("./country_indicators/cpi.csv"), DataFrame)
 
-matdata = fill(NaN, 3, 4, 112)
+# Starts at 1995-Q1 until 2019-Q4, for 99 observations
+matdata = fill(NaN, 4, 5, 99)
 for (i, country) in enumerate(countries)
-    matdata[1, i, :] = cleancountry(country, gdp, logtransform=true, difftransform=true)[1:112]
+    matdata[1, i, :] = cleancountry(country, gdp, logtransform=true, diffval=1)[17:115]
 end
 for (i, country) in enumerate(countries)
-    matdata[2, i, :] = cleancountry(country, production, logtransform=true, difftransform=true)[1:112]
+    matdata[2, i, :] = cleancountry(country, production, logtransform=true, diffval=1)[17:115]
 end
 for (i, country) in enumerate(countries)
-    matdata[3, i, :] = cleancountry(country, interestrate, difftransform=true)[1:112]
+    matdata[3, i, :] = cleancountry(country, interestrate, diffval=1)[17:115]
 end
 
-
-usa_cpi_vals = cleancountry("USA", cpi)
-deseasoned = seasonal_diff_log(usa_cpi_vals)
-adf_result = ADFTest(deseasoned[1:25], :constant, 1)
-
 for (i, country) in enumerate(countries)
-    matdata[4, i, :] = cleancountry(country, cpi)[1:116]
+    matdata[4, i, :] = cleancountry(country, cpi, logtransform=true, diffval=4)[14:112]
 end
 
 save("./globaldata.jld2", Dict("matdata" => matdata))
