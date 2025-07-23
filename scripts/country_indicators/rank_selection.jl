1849ceecc79d156afcec14b46b71e5d9eb904f6d
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2025-07-23 22:29:50 +0200

    3,1 is the optimal rank

--- scripts/country_indicators/rank_selection.jl
+++ scripts/country_indicators/rank_selection.jl
@@ -1,9 +1,21 @@
 using DrWatson
 @quickactivate :PseudoStructuralComovements
+Random.seed!(20250723)
 
 matdata = load(datadir("./country_indicators/globaldata.jld2"), "matdata");
 vecdata = tenmat(matdata, row=[1, 2])
 
 dimvals = collect(size(matdata[:, :, 1]))
+cen_data = vecdata .- mean(vecdata, dims=2)
 
 smallicest = rank_selection(vecdata, dimvals; iters=1000)
+
+res = comovement_reg(cen_data, dimvals, [3, 1]; iters=1000)
+res.res.minimum
+
+res.delta_est
+res.delta_stderr
+res.gamma_est
+res.gamma_stderr
+
+
